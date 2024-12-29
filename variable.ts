c84615e10240f005f290690f50e2c25dbade7a1d
Kunal Singh Dadhwal <155142500+Kunal-Singh-Dadhwal@users.noreply.github.com>
2024-12-29 13:53:03 +0530

    Variables ki baatcheet

--- variable.ts
+++ variable.ts
@@ -2,7 +2,12 @@ let greetings: string = "Hello Kunal";
 
 // not allowed
 // greetings = 6;
-let num: number = 5;
+let num = 5.55;
+num.toFixed(1);
 console.log(greetings);
+
+let userId = 334455;
+let isLoggedIn: boolean = false;
+
 
 export { }
