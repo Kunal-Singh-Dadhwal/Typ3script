a08626a6cf01fa0d18fc3c935e7df6e375ad38ea
Kunal Singh Dadhwal <155142500+Kunal-Singh-Dadhwal@users.noreply.github.com>
2024-12-31 11:35:32 +0530

    Enums

---
+++ enums.ts
@@ -0,0 +1,14 @@
+// const AISLE = 0;
+// const MIDDLE = 1;
+// const WINDOW = 2;
+
+
+// better way
+const enum SeatChoice {
+  AISLE,
+  MIDDLE, 
+  WINDOW
+};
+
+const userSeat = SeatChoice.AISLE;
+export { };
